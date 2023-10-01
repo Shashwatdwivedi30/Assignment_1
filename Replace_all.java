7d5e54f2176597af47d564c20c85c70f880b195f
Shashwat Dwivedi <117923310+Shashwatdwivedi30@users.noreply.github.com>
2023-10-01 23:37:57 +0530

    Create Replace_all.java

---
+++ Replace_all.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Replace_all {
+    public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        System.out.println(n.replace('0','5'));
+    }
+}
